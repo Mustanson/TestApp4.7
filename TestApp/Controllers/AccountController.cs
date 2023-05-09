ca485f58b36995cf0b20926451807d955938d07a
skhan777 <78987980+skhan777@users.noreply.github.com>
2023-05-08 23:06:09 -0500

    framework 4.7

---
+++ TestApp/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        // Sends an OpenIDConnect Sign-In Request.  
+        public void Login()
+        {
+            if (!Request.IsAuthenticated)
+            {
+
+                HttpContext.GetOwinContext()
+                    .Authentication.Challenge(new AuthenticationProperties { RedirectUri = "/" },
+                        OpenIdConnectAuthenticationDefaults.AuthenticationType);
+            }
+        }
+
+
+        // Signs the user out and clears the cache of access tokens.  
+        public void LogOut()
+        {
+
+            HttpContext.GetOwinContext().Authentication.SignOut(
+                OpenIdConnectAuthenticationDefaults.AuthenticationType, CookieAuthenticationDefaults.AuthenticationType);
+
+        }
+
+
+    }
+}
